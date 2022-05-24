f69dc3e301f0c5d91b3886a157c3f1461a76c13e
xurb <xurongbiao@hmtim.com>
2022-05-24 22:57:41 +0800

    springboot001

---
+++ springboot001/src/main/java/com/msb/Springboot001Application.java
@@ -0,0 +1,15 @@
+package com.msb;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot001Application {
+
+    //定义一个主方法
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot001Application.class,args);
+    }
+
+}
